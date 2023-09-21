c608559db6ca31f0a1010da8b8b46ebd7c08f8d0
ofisch <onni.fischer@gmail.com>
2023-09-21 12:00:45 +0300

    striimi-sivu semi jees

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
     extend: {
       scale: {
         175: "1.75",
+      },
+      px: {
+        3: "3px",
       },
     },
     colors: {
