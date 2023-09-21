a9cfbcd3fbc6b3d15a0b0419e6f4f2e9ad53b3f7
ofisch <onni.fischer@gmail.com>
2023-09-21 21:24:43 +0300

    video.js toimii + teema lisätty ja kustomisoitu sivun väreihin

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+let myPlayer = document.querySelector("#my-video");
+
+console.log(myPlayer);
+
+videojs(myPlayer, {
+  controls: true,
+  autoplay: false,
+  preload: "auto",
+  poster: "../img/banneriKuva.jpg",
+});
+*/
